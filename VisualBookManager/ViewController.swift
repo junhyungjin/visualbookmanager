aefcc9c08d3e416df5cbe130147c98413451d9ef
Hyung jin Jun <junhyungjin@me.com>
2016-04-19 23:43:52 +0900

    Initial Commit

---
+++ VisualBookManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisualBookManager
+//
+//  Created by junhyungjin on 2016. 4. 19..
+//  Copyright © 2016년 Hyung Jun Jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
